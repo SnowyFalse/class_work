cd26fe5ac288196fcf3f8e2f159f34898b0f5cca
Theresa <theresa.dietinger@gmail.com>
2022-10-21 17:23:07 +0200

    fix rotation speed of diamonds

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-
+    public float speed = 5f;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(10, 25, 35)); 
+        transform.Rotate(new Vector3(10, 25, 35) * Time.deltaTime * speed); 
     }
 }
